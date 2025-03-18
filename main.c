38febf3d08840638b55ca42584a20de915ed874d
Frederick Jeffcock <frederick.jeffcock@epitech.eu>
2025-03-18 09:57:45 +0100

    Testing chocolatine

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2025
+** test-chocolatine
+** File description:
+** main
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("Into the Deep\n");
+    return 0;
+}
